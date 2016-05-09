0f48d6b652e3099fc454930dba7cc892c2b5fb3b
mhavey <mchavey@gmail.com>
2016-05-09 05:36:47 -0400

    handle stick and fire too

--- pl/wolf_goat_cabbage.pl
+++ pl/wolf_goat_cabbage.pl
@@ -19,34 +19,47 @@ dfs(Move, Goal, S, Checked, Path) :-
 % goal is no one left on the left side
 wgcGoal([[],_]).
 
-% move - man always alternates between left and right; he can take zero or one object with him.
-% he can't leave the following pairs on the other side: (wolf,goat), (goat,cabbage)
+% move - man always alternates between left and right; he can take zero, one of two objects with him.
+% he can't leave the following pairs on the other side: (wolf,goat), (goat,cabbage), (stick,wolf), (fire,stick).
 
-% move 1 - man only moves left to right.
+% move 1a - man only moves left to right.
 wgcMove([[man|Left],Right],[Left,[man|Right]]) :- safety(Left).
 
-% move 2 - man only moves right to left.
-wgcMove([Left,[man|Right]],[[man|Left],Right]) :- safety(Right).
-
-% move 3 - man and some other object moves left to right
+% move 1b - left to right with passengers
 wgcMove([[man|Left],Right],[NewLeft,[man|NewRight]]) :- 
-	selectPassengers(Left,NewLeft,Right,NewRight),
+	choosePassengers(Left,Passengers),
+	selectPassengers(Passengers,Left,NewLeft,Right,NewRight),
 	safety(NewLeft).
 
-% move 4 - man and some other object moves right to left
+% move 2a - man only moves right to left.
+wgcMove([Left,[man|Right]],[[man|Left],Right]) :- safety(Right).
+
+% move 2b - right to left with passengers
 wgcMove([Left,[man|Right]],[[man|NewLeft],NewRight]) :- 
-	selectPassengers(Right,NewRight,Left,NewLeft),
+	choosePassengers(Left,Passengers),
+	selectPassengers(Passengers,Right,NewRight,Left,NewLeft),
 	safety(NewRight).
 
+% choose 1 or 2 passengers to take across
+choosePassengers(Departures, Passengers) :- 
+	length(Departures, DepartureLen),
+	between(1,2,NumPassengers), 
+	min_list([DepartureLen,NumPassengers],Passengers).
+
 % select a passengers to move from departure to arrival
-selectPassengers(Departure,NewDeparture,Arrival,NewArrival) :-
+selectPassengers(1,Departure,NewDeparture,Arrival,NewArrival) :-
 	member(Object,Departure),
 	delete(Departure,Object,NewDeparture),
 	append(Arrival,[Object],NewArrivalUnsorted),
 	sort(NewArrivalUnsorted,NewArrival).
+selectPassengers(2,Departure,NewDeparture,Arrival,NewArrival) :-
+	selectPassengers(1,Departure,NewDeparture1,Arrival,NewArrival1),
+	selectPassengers(1,NewDeparture1,NewDeparture,NewArrival1,NewArrival).
 
 % check a side unattended by the man is safe
 safety(Side) :-
+	noPair(Side,stick,wolf),
+	noPair(Side,fire,stick),
 	noPair(Side,wolf,goat),
 	noPair(Side,goat,cabbage).
 
@@ -60,13 +73,14 @@ hasElem(X, [Y|T], N) :-
 % check the side does NOT have both the objects	
 noPair(Pair,Object1,Object2) :- 
 	hasElem(Object1,Pair,O1Count),
-	hasElem(Object2,Pair,O2Count),
+	hasElem(Object2,Pair,O2Count),	
 	Total is O1Count + O2Count,
 	Total < 2.
 	
 % main search predicate
 wgcSearch(Path) :-
-	InitState = [[man,wolf,goat,cabbage],[]],
+	InitState = [[man,fire,stick,wolf,goat,cabbage],[]],
 	dfs(wgcMove, wgcGoal, InitState, [InitState], RevPath),
 	% dfs algorithm gives path from last to first, reverse it to give forward path back to caller
 	reverse(RevPath,Path).
+	
